7cd3eaef53ec4a6cbb39929bf824cb088c977577
Nidhi Mehta <nidhididi@gmail.com>
2014-03-03 12:15:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twist::Application.config.secret_key_base = '00be2a57c23fc088e5f61af61b9b7be9a270fd1e7efb5bbbcbd9f1223e6b6e23ade39db3e7fc694586997cd3011230b3adae57506e1702b62008d45d9a1f1b2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twist::Application.config.session_store :cookie_store, key: '_Twist_session'
