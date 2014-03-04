96c7b815df33cfa1c5a505876a9cbb2313c57cea
Nidhi Mehta <nidhididi@gmail.com>
2014-03-03 23:17:33 -0500

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Twist::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
+  resources :comments
+
+  resources :tweets
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303170215) do
+ActiveRecord::Schema.define(version: 20140303174053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "tweet_id"
+    t.string   "user_id"
+    t.text     "comment_text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tweets", force: true do |t|
+    t.integer  "user_id"
+    t.text     "tweet"
+    t.integer  "num_comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
